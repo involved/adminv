1e45e57e5a786962a47cdda037b3ccbc28df462e
Nicholas Bruning <nicholas@bruning.com.au>
2011-03-14 20:03:38 +1100

    Broke ground on view helpers - nothing much of note, yet

--- lib/adminv/helpers/view_helper.rb
+++ lib/adminv/helpers/view_helper.rb
@@ -1,7 +1,13 @@
 module Adminv
   module Helpers
     module ViewHelper
-      
+      @@width_options = {:full => 12, :three_quarter => 9, :half => 6, :quarter => 3}
+
+      def adminv_block(options = {})
+        block_class = "block"
+
+        
+      end
     end
   end
 end
