393c49ee743bcc931d06007543e273ebb6f46404
Jordan Lewis <jordan@involved.com.au>
2011-08-10 11:44:01 +1000

    Added helpers from TION - need to massage in

--- lib/adminv/helpers/view_helper.rb
+++ lib/adminv/helpers/view_helper.rb
@@ -9,6 +9,36 @@ module Adminv
       def page_id(new_id)
         content_for(:page_id){ new_id }
       end
+
+      def back_block
+        render :partial => 'manage/shared/back'
+      end
+
+      def sidebar_right(resource = nil)
+        render :partial => 'manage/shared/sidebar_right', :locals => {:resource_class => resource.class, :resource => resource}
+      end
+
+      def empty_row(collection, name, &block)
+        # should capture the block and inject into the front of the button group aswell?
+        render :partial => 'manage/shared/empty_row', :locals => {:name => name} if collection.empty?
+      end
+
+      def avatar_url(user, options = {})
+        options[:size] ||= 64
+        gravatar_id = user.is_a?(String) ? Digest::MD5::hexdigest(user).downcase : Digest::MD5::hexdigest(user.email).downcase
+        "http://gravatar.com/avatar/#{gravatar_id}.png?d=mm&r=pg&s=#{options[:size]}"
+      end
+
+      def avatar(user, options = {})
+        options[:size] ||= 64
+        image_tag avatar_url(user, options), :width => options[:size], :height => options[:size]
+      end
+
+      def boolean_for(bool)
+        image_tag bool ? 'adminv/ui-icon_status-true.png' : 'adminv/ui-icon_status-false.png'
+      end
+
     end
   end
 end
+
